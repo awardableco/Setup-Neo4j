adfb8ed81dbf0bd337fe5a14e167bfdac40294fa
Matthew Danics <matthewdanics@gmail.com>
2021-01-15 00:45:23 -0500

    update neo4j to use latest

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker_run="docker run"
-docker_run="$docker_run -d -p 7474:7474 -p 7687:7687 --env='NEO4JLABS_PLUGINS=[\"apoc\"]' --env=NEO4J_AUTH=none"
+docker_run="$docker_run -d -p 7474:7474 -p 7687:7687 --env='NEO4JLABS_PLUGINS=[\"apoc\"]' --env=NEO4J_AUTH=none neo4j:latest"
 
 sh -c "$docker_run"
