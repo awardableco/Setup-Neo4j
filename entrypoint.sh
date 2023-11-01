14f8bdfd279b1592817a1cedf81caef217af87d3
Awardable <146115377+awardableco@users.noreply.github.com>
2023-11-01 13:57:24 +0200

    Volume and auth hotfix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker_run="docker run"
-docker_run="$docker_run -d -p 7474:7474 -p 7687:7687 --volume $(pwd)/storage/app/public:/var/lib/neo4j/import --env='NEO4JLABS_PLUGINS=[\"apoc\"]' --env=NEO4J_AUTH=neo4j/$INPUT_PASSWORD --env='apoc.import.file.enabled=true' --env=NEO4J_AUTH=none neo4j:$INPUT_TAG"
+docker_run="$docker_run -d -p 7474:7474 -p 7687:7687 --volume storage/app/public:/var/lib/neo4j/import --env='NEO4JLABS_PLUGINS=[\"apoc\"]' --env=NEO4J_AUTH=neo4j/$INPUT_PASSWORD --env='apoc.import.file.enabled=true' --env='dbms.server.directories.import=/var/lib/neo4j/import' neo4j:$INPUT_TAG"
 
 sh -c "$docker_run"
