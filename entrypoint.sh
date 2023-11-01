814d3fbea1e73098e8415bea25317fb6c6f739f3
Awardable <146115377+awardableco@users.noreply.github.com>
2023-11-01 15:26:39 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker_run="docker run"
-docker_run="$docker_run -d -p 7474:7474 -p 7687:7687 --volume /home/runner/work/core/core/storage/app/phpunit:/var/lib/neo4j/import --env='NEO4JLABS_PLUGINS=[\"apoc\"]' --env=NEO4J_AUTH=none --env='apoc.import.file.enabled=true' --env='dbms.server.directories.import=/var/lib/neo4j/import' neo4j:$INPUT_TAG"
+docker_run="$docker_run -d -p 7474:7474 -p 7687:7687 --volume /home/runner/work/core/core/tests/templates/neo4j:/var/lib/neo4j/import --env='NEO4JLABS_PLUGINS=[\"apoc\"]' --env=NEO4J_AUTH=none --env='apoc.import.file.enabled=true' --env='dbms.server.directories.import=/var/lib/neo4j/import' neo4j:$INPUT_TAG"
 
 sh -c "$docker_run"
