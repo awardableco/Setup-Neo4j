d9934cb1fa8a017e8dbb286bb6020aadff59f397
Awardable <146115377+awardableco@users.noreply.github.com>
2023-11-01 14:28:21 +0200

    Removed authentication

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker_run="docker run"
-docker_run="$docker_run -d -p 7474:7474 -p 7687:7687 --volume /home/runner/work/core/core/storage/app/public:/var/lib/neo4j/import --env='NEO4JLABS_PLUGINS=[\"apoc\"]' --env=NEO4J_AUTH=neo4j/$INPUT_PASSWORD --env='apoc.import.file.enabled=true' --env='dbms.server.directories.import=/var/lib/neo4j/import' neo4j:$INPUT_TAG"
+docker_run="$docker_run -d -p 7474:7474 -p 7687:7687 --volume /home/runner/work/core/core/storage/app/public:/var/lib/neo4j/import --env='NEO4JLABS_PLUGINS=[\"apoc\"]' --env=NEO4J_AUTH=none --env='apoc.import.file.enabled=true' --env='dbms.server.directories.import=/var/lib/neo4j/import' neo4j:$INPUT_TAG"
 
 sh -c "$docker_run"
