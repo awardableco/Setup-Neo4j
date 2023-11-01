83c6eabe5d4d9231db94ae6d4de660c6a84bbe85
Awardable <146115377+awardableco@users.noreply.github.com>
2023-11-01 07:43:05 +0200

    Enabling file imports

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker_run="docker run"
-docker_run="$docker_run -d -p 7474:7474 -p 7687:7687 --env='NEO4JLABS_PLUGINS=[\"apoc\"]' --env=NEO4J_AUTH=none neo4j:$INPUT_TAG"
+docker_run="$docker_run -d -p 7474:7474 -p 7687:7687 --env='NEO4JLABS_PLUGINS=[\"apoc\"]' --env='apoc.import.file.enabled=true' --env='dbms.server.directories.import=ABSOLUTE_DIRECTORY_PATH' --env=NEO4J_AUTH=none neo4j:$INPUT_TAG"
 
 sh -c "$docker_run"
